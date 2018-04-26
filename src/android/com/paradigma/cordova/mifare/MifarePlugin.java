47ddd4e7ace422fec97092d0aefdb24c6a5511b3
Oscar Ferrer <oferrer@paradigmadigital.com>
2018-04-26 14:24:52 +0200

    Added timeout to Plugin

--- src/android/com/paradigma/cordova/mifare/MifarePlugin.java
+++ src/android/com/paradigma/cordova/mifare/MifarePlugin.java
@@ -7,6 +7,7 @@ import org.json.JSONException;
 import com.rfid.reader.Reader;
 import java.io.IOException;
 import java.security.InvalidParameterException;
+import java.util.concurrent.*;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.util.Log;
@@ -14,21 +15,50 @@ import android.util.Log;
 public class MifarePlugin extends CordovaPlugin {
 
     public Reader reader = null;
+    private final ExecutorService readUIDExecutor;
     private static final String TAG = "MifarePlugin";
+    private static final Integer TIMEOUT_SECONDS_DEFAULT = 10;
     private static final String DEVICE = "/dev/ttyS3"; // UART serial port
 
+    public MifarePlugin(){
+        super();
+        readUIDExecutor= Executors.newSingleThreadExecutor();
+    }
     
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
 
+        
         if (action.equals("readUID")) {
-
             cordova.getThreadPool().execute(new Runnable() {
                 public void run() {
-                    String uid = getUID();
-                    PluginResult result = new PluginResult(PluginResult.Status.OK, uid);
-                    result.setKeepCallback(true);
-                    callbackContext.sendPluginResult(result);       
+                    
+                    Future<String> future = readUIDExecutor.submit(new Callable() {
+                        
+                        public String call() throws Exception {
+                            String uid = getUID();
+                            PluginResult result = new PluginResult(PluginResult.Status.OK, uid);
+                            result.setKeepCallback(true);
+                            callbackContext.sendPluginResult(result);      
+                            return uid ;
+                        }
+                    });
+                    try {
+                        // Set timeout
+                        int timeout = args.length() != 0 ? args.getJSONObject(0).getInt("timeout") : TIMEOUT_SECONDS_DEFAULT;
+                        Log.d(TAG, "*** Before future.get()..." + timeout);
+                        future.get(timeout, TimeUnit.SECONDS); 
+                    } catch (TimeoutException e) {
+                        Log.d(TAG, "Mifare timeout");
+                        PluginResult result = new PluginResult(PluginResult.Status.ERROR, "Mifare timeout");
+                        callbackContext.sendPluginResult(result);
+                    } catch (Exception e) {
+                        Log.d(TAG, "Mifare error" + e.getMessage());
+                        PluginResult result = new PluginResult(PluginResult.Status.ERROR, "Mifare error");
+                        callbackContext.sendPluginResult(result);
+                    } finally {
+                        readUIDExecutor.shutdownNow();
+                    }
                 }
             });
             return true;
@@ -41,36 +71,31 @@ public class MifarePlugin extends CordovaPlugin {
 
     private Reader getReader() throws SecurityException, IOException, InvalidParameterException {
         if (reader == null) {
-			// Get reader from serial port
             reader = Reader.getInstance(DEVICE, 9600);
         }
-        Log.d(TAG, "getReader() before return() ");
 
         return reader;
     }
 
-    private String getUID(){
+    private String getUID() throws IOException,SecurityException,InvalidParameterException{
         byte[] uid = new byte[32];
         byte[] uidLen = new byte[1];
         byte[] errCode = new byte[1];
-        try {
-            // Read until get response.
-            int result = getReader().Iso14443a_GetUid(uid, uidLen, errCode);
-            while (result != 0){
-                result = getReader().Iso14443a_GetUid(uid, uidLen, errCode);
-            }
 
-            if (result != 0) {
-                return "GetUid Error, errCode=" + String.format("%02X", (byte) errCode[0]);
-            } else {
-                String strUid = "";
-                for (int i = 0; i < uidLen[0]; i++) {
-                    strUid += String.format("%02X ", uid[i]);
-                }
-                return strUid;
+        // Read until get response.
+        int result = getReader().Iso14443a_GetUid(uid, uidLen, errCode);
+        while (result != 0){
+            result = getReader().Iso14443a_GetUid(uid, uidLen, errCode);
+        }
+
+        if (result != 0) {
+            return "GetUid Error, errCode=" + String.format("%02X", (byte) errCode[0]);
+        } else {
+            String strUid = "";
+            for (int i = 0; i < uidLen[0]; i++) {
+                strUid += String.format("%02X ", uid[i]);
             }
-        } catch (Exception e){
-            return ("Cannot get reader");
+            return strUid;
         }
     }
 }
--- www/mifare.js
+++ www/mifare.js
@@ -1,13 +1,13 @@
 "use strict";
 
 var mifare ={
-    readUID: function(onSuccess){
+    readUID: function(onSuccess, onError, args){
         cordova.exec(
             onSuccess,
-            function (reason) {
-                console.log( "onFailure Plugin " + reason);
-            },
-            "MifarePlugin", "readUID"
+            onError,
+            "MifarePlugin", 
+            "readUID", 
+            [args]
         );
     }
 }
